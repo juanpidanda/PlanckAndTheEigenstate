fa7eb10a5627ad212f35fd7ec305ad67347a366a
AugustoCogollo <augusto.wyvern312@hotmail.com>
2023-09-16 17:02:38 -0600

    Límite a la velocidad del jugador

--- PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/Player/CharacterController.cs
+++ PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/Player/CharacterController.cs
@@ -8,6 +8,7 @@ public class CharacterController : MonoBehaviour
     [Header("Movement")]
     public float movementSpeed;
     public ForceMode2D movementForceMode;
+    public float maxMovementSpeed;
     [Header("Jumping")]
     [SerializeField]bool jumpAvailable;
     //public float jumpCooldown;
@@ -41,7 +42,10 @@ public class CharacterController : MonoBehaviour
     }
     void Movement(float direction)
     {
-        characterRigidbody.AddForce(movementSpeed * direction * Vector2.right, movementForceMode);
+        Vector2 movement = movementSpeed * direction * Vector2.right;
+        characterRigidbody.AddForce(movement, movementForceMode);
+        Debug.Log("Magnitude: " + characterRigidbody.velocity.magnitude.ToString());
+        characterRigidbody.velocity = truncate(characterRigidbody.velocity, maxMovementSpeed);
     }
     void Jump()
     {
@@ -67,5 +71,16 @@ public class CharacterController : MonoBehaviour
     //    jumpAvailable = true;
 
     //}
-    
+    private static Vector3 truncate(Vector3 vector, float maxValue)
+    {
+        //Condicional Sentinela
+        if (vector.magnitude <= maxValue)
+        {
+            return vector;
+        }
+
+        vector.Normalize();
+        return vector *= maxValue;
+    }
+
 }
