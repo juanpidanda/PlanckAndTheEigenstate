4a99dada049094cf3f42866a60c351f3d23a7083
LAPURACHULETA <101416312+LAPURACHULETA@users.noreply.github.com>
2023-09-16 17:34:04 -0600

    ...
    ...

--- PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/Plataforma360.cs
+++ PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/Plataforma360.cs
@@ -7,7 +7,7 @@ public class Plataforma360 : MonoBehaviour
     [SerializeField] private Transform pivot;
     [SerializeField] private float speed;
   
-    void Update()
+    void FixedUpdate()
     {
         this.transform.RotateAround(pivot.position,Vector3.forward, speed * Time.deltaTime);
         this.transform.Rotate(Vector3.forward, -speed * Time.deltaTime);
