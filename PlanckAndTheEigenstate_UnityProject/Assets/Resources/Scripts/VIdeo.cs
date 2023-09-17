e4cca9c35af4975038b70bce50820bb40c9f74fa
LAPURACHULETA <101416312+LAPURACHULETA@users.noreply.github.com>
2023-09-17 02:04:03 -0600

    Video Intro
    se implemento la intro del juego

---
+++ PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/VIdeo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VIdeo : MonoBehaviour
+{
+    private float timeVideo;
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeVideo = 26.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        Debug.Log(currentTime);
+        if (currentTime >= timeVideo)
+        {
+            Application.LoadLevel("MainMenu");
+        }
+
+    }
+}
