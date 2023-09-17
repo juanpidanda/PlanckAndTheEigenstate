3a3822fd151c37f9cd7e00da43e753418806dcfd
chuyxxy <chuyxxy@gmail.com>
2023-09-17 03:42:45 -0600

    camara
    1

--- PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/ButtonsBehaviours.cs
+++ PlanckAndTheEigenstate_UnityProject/Assets/Resources/Scripts/ButtonsBehaviours.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsBehaviours : MonoBehaviour
 {
     public void CallLevelScene(int level)
     {
-        GameManager.gameManagerInstance.CallGameScene(level);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void PauseButton(bool wantToPause)
     {
